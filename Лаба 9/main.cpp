041d9670b09d37836cbfd925fd74ba49b31084a5
tompavlo <tompavlo@ukr.net>
2024-04-20 22:57:47 +0300

    Continue working on lab 9(graphs)

--- Лаба 9/main.cpp
+++ Лаба 9/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
+bool isCompleteGraph(int graph[4][4], int size) {
+
+    for(int i=0; i<size; i++) {
+        for(int j=0; j<size; j++) {
+            if(i!=j && graph[i][j]==0) return false;
+        }
+    }
+
+    return true;
+}
 
 int main() {
     int graph[4][4] = {
@@ -8,5 +18,5 @@ int main() {
         {4, 6, 0, 7},
         {0, 3, 7, 0}
     };
-    cout << graph;
+    cout <<"Чи граф повний?\n"<<boolalpha<< isCompleteGraph(graph,4);
 }
