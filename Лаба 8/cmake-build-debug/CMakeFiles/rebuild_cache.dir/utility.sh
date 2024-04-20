f6b1905af452b2cd3c4099fec3b351c8eedc8b8b
tompavlo <tompavlo@ukr.net>
2024-04-21 00:50:56 +0300

    problems with gitignore

---
+++ Лаба 8/cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd "/cygdrive/c/Users/MSI/Desktop/Clion/Структури даних та алгоритмів/CPP/Лаба 8/cmake-build-debug"
+/cygdrive/c/Users/MSI/AppData/Local/JetBrains/CLion2024.1/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
