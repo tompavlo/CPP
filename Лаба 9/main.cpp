a6091ccbb34fdb97e2a33af55ef3a7bbf29a6d42
tompavlo <tompavlo@ukr.net>
2024-04-20 22:43:33 +0300

    Started working on lab 9(graphs)

--- Лаба 9/main.cpp
+++ Лаба 9/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int graph[4][4] = {
+        {0, 0, 4, 0},
+        {0, 0, 6, 3},
+        {4, 6, 0, 7},
+        {0, 3, 7, 0}
+    };
+    cout << graph;
 }
