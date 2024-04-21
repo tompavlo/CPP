2835544bfa9d2cc69a05f64d4a855166349d0af5
tompavlo <tompavlo@ukr.net>
2024-04-21 13:15:04 +0300

    really finally fixed😎

--- Лаба 9/main.cpp
+++ Лаба 9/main.cpp
@@ -2,11 +2,11 @@
 
 #include "../../../../../../../cygwin64/usr/include/limits.h"
 using namespace std;
-bool isCompleteGraph(int graph[4][4], int size) {
 
-    for(int i=0; i<size; i++) {
-        for(int j=0; j<size; j++) {
-            if(i!=j && graph[i][j]==0) return false;
+bool isCompleteGraph(int graph[4][4], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i != j && graph[i][j] == 0) return false;
         }
     }
 
@@ -14,51 +14,52 @@ bool isCompleteGraph(int graph[4][4], int size) {
 }
 
 void printAdjacencylist(int graph[4][4], int size) {
-    cout<<"Adjacency list:\n";
-    for(int i=0; i<size; i++) {
-        cout<<i+1<<": ";
-        for(int j=0; j<size; j++) {
-            if(graph[i][j]!=0) cout<<j+1<<' ';
+    cout << "Adjacency list:\n";
+    for (int i = 0; i < size; i++) {
+        cout << i + 1 << ": ";
+        for (int j = 0; j < size; j++) {
+            if (graph[i][j] != 0) cout << j + 1 << ' ';
         }
-        cout<<endl;
+        cout << endl;
     }
 }
+
 double avgLength(int graph[4][4], int size) {
-    double result=0;
-    int counter=0;
-    for(int i=0; i<size; i++) {
-        for(int j=i; j<size; j++) {
-            if(graph[i][j]!=0) {
-                result+=graph[i][j];
+    double result = 0;
+    int counter = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = i; j < size; j++) {
+            if (graph[i][j] != 0) {
+                result += graph[i][j];
                 counter++;
             }
         }
     }
-    return result/counter;
+    return result / counter;
 }
 
 void printIzolated(int graph[4][4], int size) {
-    bool isIzolated;
-    int counter=0;
+    bool isIzolated=false;
     int closest;
-    for(int i=0; i<size; i++) {
-        isIzolated=false;
-        for(int j=0; j<size; j++) {
-            if(graph[i][j]!=0) {
-                if(isIzolated) {
-                    isIzolated=false;
+    int counter=0;
+    for (int i = 0; i < size; i++) {
+        isIzolated = false;
+        for (int j = 0; j < size; j++) {
+            if (graph[i][j] != 0) {
+                if (isIzolated) {
+                    isIzolated = false;
+                    counter--;
                     break;
                 }
-                else {
-                    isIzolated=true;
-                    closest=graph[i][j];
-                }
+                isIzolated = true;
+                counter++;
+                closest = graph[i][j];
             }
         }
-        if(counter<2);
+        if (isIzolated) cout<<"Node " << i + 1 << " is izolated, distance to closest object " << closest;
     }
-
 }
+
 int main() {
     int graph[4][4] = {
         {0, 0, 4, 0},
@@ -66,8 +67,9 @@ int main() {
         {4, 6, 0, 7},
         {0, 3, 7, 0}
     };
-    cout <<"Чи граф повний?\n"<<boolalpha<< isCompleteGraph(graph,4)<<endl;
+    cout << "Чи граф повний?\n" << boolalpha << isCompleteGraph(graph, 4) << endl;
     printAdjacencylist(graph, 4);
-    cout<<"Average lenght: "<<avgLength(graph, 4)<<endl<<endl;
-
+    cout << "Average lenght: " << avgLength(graph, 4) << endl << endl;
+    cout<<"Izolated:\n";
+    printIzolated(graph, 4);
 }
