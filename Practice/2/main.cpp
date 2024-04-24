6a1e4bb86d6150a980856e27fa0cc55fbeb39cbd
tompavlo <tompavlo@ukr.net>
2024-04-24 23:25:27 +0300

    Ended practicing for tomorrow

--- Practice/2/main.cpp
+++ Practice/2/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <queue>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 int main() {
     cout<<"Total clients: ";
     int size;
     cin>>size;
+    queue<int> clients;
+    for(int i=1; i<size; i++) {
+        clients.push(i);
+    }
+    while(!clients.empty()) {
+        cout<<"Було обслуговано клієнта №"<<clients.front()<<endl;
+        clients.pop();
+        this_thread::sleep_for(chrono::seconds(1));
+    }
     return 0;
 }
--- Лаба 7/cmake-build-debug/cmake_install.cmake
+++ Лаба 7/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/MSI/Desktop/Clion/Структури даних та алгоритмів/Лаба 7
+# Install script for directory: /cygdrive/c/Users/MSI/Desktop/Clion/Структури даних та алгоритмів/CPP/Лаба 7
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/_7")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/MSI/AppData/Local/Programs/CLion Nova/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/MSI/Desktop/Clion/Структури даних та алгоритмів/Лаба 7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/Users/MSI/Desktop/Clion/Структури даних та алгоритмів/CPP/Лаба 7/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
