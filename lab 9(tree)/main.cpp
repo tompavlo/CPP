9bba7160a7c1f616772435d5c2f99265df7a5045
tompavlo <tompavlo@ukr.net>
2024-05-07 22:08:27 +0300

    Done on labs about trees

--- lab 9(tree)/main.cpp
+++ lab 9(tree)/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 struct tree {
     int key;
     tree *left;
@@ -14,54 +15,96 @@ tree *createNode(int data) {
     return newNode;
 }
 
-void input(int data) {
-    tree *newNode = createNode(data);
-    if (root == nullptr) root = newNode;
-    else {
-        tree *manip = root;
-        while (manip != nullptr) {
-            if (newNode->key >= manip->key) {
-                if (manip->right == nullptr) {
-                    manip->right = newNode;
-                    break;
-                }
-                manip = manip->right;
-            } else {
-                if (manip->left == nullptr) {
-                    manip->left = newNode;
-                    break;
-                }
-                manip = manip->left;
-            }
-        }
-    }
-}
-void inputResurcivly(tree*& node, int data) {
-    if(!node) {
-        node = createNode(data);
-    }
-    else if (data< node->key) {
-        inputResurcivly(node->left, data);
-    }
+void inputResurcivly(tree *&node, int data) {
+    if (!node) node = createNode(data);
+    else if (data < node->key) inputResurcivly(node->left, data);
     else inputResurcivly(node->right, data);
 }
 
-void printTree(tree *root) {
-    if(!root) return;
-    printTree(root->left);
-    cout<<root->key<<' ';
-    printTree(root->right);
-
+bool checkForTheSameValue(tree *&root, int key) {
+    if (!root) return false;
+    if (key == root->key) return true;
+    return checkForTheSameValue(root->left, key) || checkForTheSameValue(root->right, key);
 }
 
+void createNodes(int size, bool manually) {
+    if (manually) {
+        cout << "Input data" << endl;
+        for (int i = 0; i < size; i++) {
+            int data;
+            cin >> data;
+            while (checkForTheSameValue(root, data)) {
+                cout << "There is already in tree this key, input again\n";
+                cin >> data;
+            }
+            inputResurcivly(root, data);
+        }
+    } else {
+        cout << "Input diapazon: ";
+        int start, end;
+        cin >> start >> end;
+        if (start > end) swap(start, end);
+        while (size > end - start + 1) {
+            cout << "Incorect diapazon, input again: ";
+            cin >> start >> end;
+            if (start > end) swap(start, end);
+        }
+        for (int i = 0; i < size; i++) {
+            int data = rand() % (end - start + 1) + start;
+            while (checkForTheSameValue(root, data)) {
+                data = rand() % (end - start + 1) + start;
+            }
+            inputResurcivly(root, data);
+        }
+    }
+}
+
+void createEvenAndNotEvenTrees(tree *mainRoot, tree*&rootEven, tree *&rootNotEven) {
+    if (!mainRoot) return;
+    if (mainRoot->key % 2 == 0) inputResurcivly(rootEven, mainRoot->key);
+    else inputResurcivly(rootNotEven, mainRoot->key);
+    createEvenAndNotEvenTrees(mainRoot->left, rootEven, rootNotEven);
+    createEvenAndNotEvenTrees(mainRoot->right, rootEven, rootNotEven);
+}
+
+void printTree(tree *root) {
+    if (!root) return;
+    printTree(root->left);
+    cout << root->key << ' ';
+    printTree(root->right);
+}
+
+int depthOfTree(tree *root) {
+    if (root==nullptr) return 0;
+    return 1 + max(depthOfTree(root->left), depthOfTree(root->right));
+}
+
+void deleteTree(tree*& root) {
+    if(!root) return;
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+}
 int main() {
-    inputResurcivly(root, 1);
-    inputResurcivly(root, 16);
-    inputResurcivly(root, 1);
-    inputResurcivly(root, 17);
-    inputResurcivly(root, 6);
-    inputResurcivly(root, 3);
-    inputResurcivly(root, 18);
-    inputResurcivly(root, 5);
+    cout << "Total nodes: ";
+    int size;
+    cin >> size;
+    cout << "Input mannualy?" << endl;
+    bool man;
+    cin >> man;
+    srand(time(nullptr));
+    createNodes(size, man);
     printTree(root);
+    tree *even = nullptr, *notEven = nullptr;
+    createEvenAndNotEvenTrees(root, even, notEven);
+    cout << endl;
+    printTree(even);
+    cout << endl;
+    printTree(notEven);
+    cout<<"\nDepth of main tree: "<<depthOfTree(root)<<endl;
+    cout<<"Depth of even tree: "<<depthOfTree(even)<<endl;
+    cout<<"Depth of not even tree: "<<depthOfTree(notEven)<<endl;
+    deleteTree(root);
+    deleteTree(notEven);
+    deleteTree(even);
 }
